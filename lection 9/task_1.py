60c309b2b807a9868b4e2d4728befa06fc337781
dv.savvateeva <dv.savvateeva@tensor.ru>
2023-06-06 00:54:50 +0300

    lesson 9 done

---
+++ lection 9/task_1.py
@@ -0,0 +1,20 @@
+# Дан текстовый файл test_file/task1_data.txt
+# Он содержит текст, в словах которого есть цифры.
+# Необходимо удалить все цифры и записать получившийся текст в файл test_file/task1_answer.txt
+
+
+with open('test_file/task1_data.txt', mode='r', encoding='utf-8') as file_start:
+    with open('test_file/task1_answer.txt', mode='w', encoding='utf-8') as file_finish:
+        for line in file_start:
+            line_without_digits = "".join(i for i in line if not i.isdigit())
+            file_finish.write(line_without_digits)
+
+# Ниже НИЧЕГО НЕ НАДО ИЗМЕНЯТЬ
+
+
+with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
+    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+        answer = file1.readlines()
+        ethalon = file2.readlines()
+        assert answer == ethalon, "Файл ответа не совпадает с эталонном"
+print('Всё ок')
